1cc01202a8f8bd3ef8901eeed028e9d2ce59bbf1
Riva'i Amin <raysaber37@gmail.com>
2025-05-04 20:30:39 +0700

    fixed: handling empty keyword for metadata

--- resources/views/layouts/metadata.blade.php
+++ resources/views/layouts/metadata.blade.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8">
 <meta name="author" content="{{ setting('seo.title') }}">
 <meta name="description" content="{{ setting('seo.description') }}">
-<meta name="keywords" content="{{ implode(',', setting('seo.keywords')) }}">
+<meta name="keywords" content="{{ implode(',', setting('seo.keywords', [])) }}">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap">
