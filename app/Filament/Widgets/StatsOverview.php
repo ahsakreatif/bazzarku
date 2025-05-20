52e72cc18d4e6adb524c5e9673d9c6fb2f8ab624
Riva'i Amin <raysaber37@gmail.com>
2025-05-21 04:52:32 +0700

    updated: change statistic on dashboard

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -10,12 +10,18 @@ class StatsOverview extends BaseWidget
     protected function getStats(): array
     {
         return [
-            Stat::make('User Tenant', 'heroicon-o-user-group')
-                ->value(\App\Entities\UserTenant::count()),
+            /* Stat::make('User Tenant', 'heroicon-o-user-group')
+                ->value(\App\Entities\UserTenant::count()), */
             Stat::make('User Vendor', 'heroicon-o-user')
                 ->value(\App\Entities\UserVendor::count()),
             Stat::make('Event', 'heroicon-o-calendar')
                 ->value(\App\Entities\Event::count()),
+            Stat::make('Commodity', 'heroicon-o-archive-box')
+                ->value(\App\Entities\Commodity::count()),
+            Stat::make('Applications', 'heroicon-o-cube')
+                ->value(\App\Entities\Application::count()),
+            Stat::make('Rentals', 'heroicon-o-archive-box')
+                ->value(\App\Entities\Rental::count()),
         ];
     }
 }
